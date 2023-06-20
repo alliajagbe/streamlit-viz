a643b80dad77a9aea756ce71be2d1fb3023829cf
alliajagbe <alliajagbe001@gmail.com>
2023-06-20 11:18:49 +0530

    page configurations

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.graph_objects as go
 
+
+# page configurations
 incomes = ["salary", "dividends", "lottery"]
 expenses = ["rent", "food", "bills", "saving", "fun", "clothes"]
 currency = "USD"
