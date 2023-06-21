9c46ab27a9b798e8c3fbb2df52c9bb61877da783
alliajagbe <alliajagbe001@gmail.com>
2023-06-21 15:47:50 +0530

    hiding streamlit style

--- streamlit.py
+++ streamlit.py
@@ -19,6 +19,15 @@ st.title(page_title + " " + page_icon)
 years = [datetime.today().year, datetime.today().year + 1]
 months = [calendar.month_name[i] for i in range(1, 13)]
 
+# hiding streamlit style
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+
 # navigation bar
 selected = option_menu(
     menu_title = None,
