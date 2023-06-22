081369db057b7c94091a19084c77fcd004a1b2b8
alliajagbe <alliajagbe001@gmail.com>
2023-06-22 21:55:42 +0530

    example

---
+++ example.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import plotly.graph_objects as go
+import calendar
+from datetime import datetime
+from streamlit_option_menu import option_menu
+import database as db
+
+
+# page configurations
+incomes = ["salary", "dividends", "lottery"]
+expenses = ["rent", "food", "bills", "saving", "fun", "clothes"]
+currency = "Naira"
+page_title = "Budget App"
+page_icon = "💰"
+layout = "centered"
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
+
+years = [datetime.today().year, datetime.today().year + 1]
+months = [calendar.month_name[i] for i in range(1, 13)]
