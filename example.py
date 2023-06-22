19aa41eaa7307e477357824235c9ad11aebc7307
alliajagbe <alliajagbe001@gmail.com>
2023-06-22 21:56:41 +0530

    streamlit style hidden

--- example.py
+++ example.py
@@ -19,3 +19,12 @@ st.title(page_title + " " + page_icon)
 
 years = [datetime.today().year, datetime.today().year + 1]
 months = [calendar.month_name[i] for i in range(1, 13)]
+
+# hiding streamlit style
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
